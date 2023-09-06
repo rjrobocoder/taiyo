c40119003b09d78842c6dd23850fe99c38a2d72b
rjrobocoder <rjrobocoder@gmail.com>
2023-09-06 18:39:42 +0530

    Added asset images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import ChartsAndMapsPage from "./pages/ChartsAndMapsPage";
+import MyAccountPage from "./pages/MyAccountPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline text-red-300">Hello world!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ContactPage />} />
+        <Route path="/charts-and-maps" element={<ChartsAndMapsPage />} />
+        <Route path="/myaccount" element={<MyAccountPage />} />
+      </Routes>
+    </Router>
   );
 }
 
